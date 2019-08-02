ef8b1feb3b3133e149af21f4a8da871c41fe1da3
Chandan Raj <chandanraj1919@gmail.com>
2019-08-02 23:08:28 +0000

    Automatically backed up by Learn

--- lib/greeting.rb
+++ lib/greeting.rb
@@ -2,5 +2,6 @@
 
 def greeting(name)
   puts "Hello #{name}. It's nice to meet you."
-  puts "Hello #{name}. It's nice to meet you.".strip
+  s = "Hello #{name}. It's nice to meet you.".strip
+  puts s
 end
